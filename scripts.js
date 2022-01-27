58559030be2b9d47a8dcede4e5aa08285784a184
lucasnix <lucasniix@gmail.com>
2022-01-27 20:02:45 -0300

    Commit 1

---
+++ scripts.js
@@ -0,0 +1,55 @@
+/*VARIAVEIS
+console.log('lucas')
+console.log('bem vindo ao projekt de JS')
+let idade = 5;
+console.log(idade);
+let dedos = 10;
+console.log(dedos)
+let altura = 180;
+console.log(altura)
+
+const programa = 'jojo';
+console.log(programa)
+variaveis*/
+
+/* O B J E T O S
+TIPOS PRIMITIVOS - TIPOS DE REFERENCIA E VALORES
+STRING, NUMBER, BOOLEAN, UNDEFINED, NULL E VALUE 
+let nome = 'lucas'; //string literal e o que tem dentro das aspas pode ser de qualquer forma
+let idade = 32; //string de number e o numero que vai dentro
+let estaAprovado = true; //string de boolean que constroi regras no app
+let corSelecionado = null; //onde ha redefinir um valor quando acessado
+
+let pessoa = {
+    nome: 'lucas',
+    idade: 32,
+    estaAprovado: true,
+    corCarro: 'branco',
+    altura: 182,
+    docePreferido: 'xota'
+}
+
+console.log(pessoa)*/
+
+/* A R R A Y S - CONJUNTO DE DADOS QUE E ACESSADO PELO INDICE, LOCAL EXATO ONDE ESTA A INFORMACAO
+
+let familia = [true, 32, 'Lucas', 55];
+console.log(familia[0])
+
+let nomeDoColega = ['joao','oliveira','zain','jhonson']
+console.log(nomeDoColega[2])*/
+
+/*F U N C O E S = VERBO + SUBSTANTIVO
+let corSite = "azul";
+function resetaCor(cor, tonalidade){
+    corSite = cor +' '+ tonalidade;
+};
+
+console.log(corSite);
+resetaCor("verde","claro");
+console.log(corSite);
+*/
+function dizerNome(){
+    console.log('eduardo')
+}
+dizerNome();
