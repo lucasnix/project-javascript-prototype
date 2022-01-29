8c3cac8c9889f6dffaa205c144f3fdac669a86b4
lucasnix <lucasniix@gmail.com>
2022-01-29 09:46:56 -0300

    commit 29-01-22

--- scripts3.js
+++ scripts3.js
@@ -10,7 +10,7 @@ console.log(a);
 console.log(b);*/
 
 //CONNDICIONAIS
-//IF 
+//IF
 //SE A HORA ESTIVER ENTRE AS 06:00 ATE 12:00 : BOM DIA!
 //SE ESTIVER ENTRE 12:00 ATE AS 18:00 : BOA TARDE
 //SE ESTIVER MAIS DE 18:00 : BOA NOITE
@@ -31,7 +31,99 @@ else {
 
 //ELSE*/
 
-
 //SWITCH CASE
+// usuario comum, gerente, diretor
+/*let permissao;
+permissao = 'diretor'
+switch (permissao) {
+    case 'comum':
+    console.log('usuario comum')
+    break;
+
+    case 'gerente':
+    console.log('usuario gerente')
+    break;
+
+    case 'diretor':
+    console.log('usuario diretor')
+    break;
+
+    default:
+        console.log('usuario nao reconhecido')
+}*/
+
+//LACO/LOOP FOR
+//ESCREVER ESTOU APRENNDENDO 5 VEZES
+// FOR
+
+/*for(let i = 5; i >= 1; i--) {
+    //console.log('estou apredendo', i);
+   if(i % 2 !==0) {
+       console.log(i);
+   } // i modules 2, o restante da divisao do primeiro 
+   //valor com o segundo for diferente de zero
+}*/
+
+// WHILE
+
+/*let i = 5;
+
+while (i >= 1) {
+    if(i % 2 !==0){
+        console.log(i);    
+    }
+    i--;    
+}*/
+
+// DO WHILE
+/*let i = 0;
+do {
+  console.log("digitando", i);
+  i++;
+} while (i < 10);*/
+
+//FOR.. IN
+
+/*const pessoa = {
+    nome: 'lucas',
+    idade: 32
+};
+//chave = key-value , chave e valor nome=chave lucas=valor
+for(let chave in pessoa){
+    console.log(chave, pessoa['nome']);
+}
+
+const cores = ['Vermelho', 'azul', 'verde'];
+
+for (let indice in cores) {
+    console.log(indice, cores[indice])
+}*/
 
 
+//FOR.. OF
+/*const cores = ['Vermelho', 'azul', 'verde'];
+for (let cor of cores)
+    console.log(cor);*/
+
+/*const tempo = ['sol', 'chuva', 'neblina'];
+for (let clima of tempo)
+    console.log(clima);*/
+
+/*let clima;
+clima = 'neblina'
+switch (clima) {
+    case 'sol':
+    console.log('esta sol')
+    break;
+
+    case 'chuva':
+    console.log('esta chuvoso')
+    break;
+
+    case 'neblina':
+    console.log('esta neblinando')
+    break;
+
+    default:
+        console.log('tempo indefinido')
+}*/
